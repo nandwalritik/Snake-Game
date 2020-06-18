25d80aeebb7a6916feff790b4670c361840432fb
nandwalritik <ritiknandwal021@gmail.com>
2020-06-19 01:59:21 +0530

    adding files for snake game

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+app.use(express.static(__dirname+'/public/'));
+app.get('/',(req,res)=>{
+    res.sendFile('index.html')
+});
+app.listen(port,()=> console.log(`App is listening on port ${port}`));
